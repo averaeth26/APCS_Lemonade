ceb42fdcc73e0a37f6409213893b81a2700bfd89
Ethan Avera <averaeth26@7hills.org>
2023-09-18 13:10:12 -0400

    Began working on setup class:
    Setup prompts the user for the number of cups, ice cubes, sugar packets, and lemons they want.
    It then subtracts that amount from the budget (unfinished).

--- Introduction.java
+++ Introduction.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Introduction {
     public void intro() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("What's your name: ");
+        System.out.print("What's your name?: ");
         String name = scan.nextLine();
         System.out.println("Hello, " + name + ". Welcome to Lemonade stand!");
     }
--- Setup.java
+++ Setup.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class Setup {
+    public void buyResources(int currentBudget) {
+        Scanner scan = new Scanner(System.in);
+        int budget = currentBudget;
+        System.out.print("How many cups would you like to buy today? ($0.80 per): ");
+        int numCups = scan.nextInt();
+        budget -= 0.8 * numCups;
+        System.out.print("How many lemons would you like to buy today? ($1.20 per): ");
+        int numLemons = scan.nextInt();
+        System.out.print("How many sugar packets would you like to buy today? ($1.75 per): ");
+        int numSugar = scan.nextInt();
+        System.out.print("How many ice cubes would you like to buy today? ($0.20 per): ");
+        int numIceCubes = scan.nextInt();
+
+
+        
+
+
+    }
 }
