a46cdbb015a572cd7567bed4b1b4a37582e63506
Ethan Avera <averaeth26@7hills.org>
2023-09-18 12:53:09 -0400

    Finished first draft of introduction class
    Class asks for user's name and prints a personalized welcome message for them

--- Introduction.java
+++ Introduction.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Introduction {
-    public static void intro() {
+    public void intro() {
         Scanner scan = new Scanner(System.in);
+        System.out.print("What's your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name + ". Welcome to Lemonade stand!");
     }
     
 }
