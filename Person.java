d445a92a00770e6fdacf9731e0351704eda3853a
Ethan Avera <averaeth26@7hills.org>
2023-09-22 13:21:27 -0400

    Created the person class,  working on randomly assigning attributes.

--- Person.java
+++ Person.java
@@ -1,3 +1,7 @@
+import java.lang.Math;
 public class Person {
-    
+    double pricePoint = Math.random();
+    int minIce = (int)(Math.random()*11) + (int)();
+    double minSugar = (int)(Math.random()*4);
+    int numLemons = (int)(Math.random()*5);
 }
--- Setup.java
+++ Setup.java
@@ -8,14 +8,14 @@ public class Setup {
         budget -= 0.8 * numCups;
         System.out.print("How many lemons would you like to buy today? ($1.20 per): ");
         int numLemons = scan.nextInt();
+        budget -= 1.2 * numLemons;
         System.out.print("How many sugar packets would you like to buy today? ($1.75 per): ");
         int numSugar = scan.nextInt();
+        budget -= 1.75 * numSugar;
         System.out.print("How many ice cubes would you like to buy today? ($0.20 per): ");
         int numIceCubes = scan.nextInt();
-
+        budget -= 0.2 * numIceCubes;
 
         
-
-
     }
 }
